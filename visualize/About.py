ee4f5136dcf3ef6ab9492273f08d5a504f5edcf5
HapizTeoh <hapiz.teoh@gmail.com>
2025-05-11 22:43:23 +0800

    Add login page feature

--- visualize/About.py
+++ visualize/About.py
@@ -26,3 +26,15 @@ st.markdown(
     - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
 """
 )
+
+def login_screen():
+    st.header("This app is private.")
+    st.subheader("Please log in.")
+    if st.button("Log in with Google"):
+        st.login()
+
+if not st.user.is_logged_in:
+    login_screen()
+else:
+    st.header(f"Welcome, {st.user.name}!")
+    st.button("Log out", on_click=st.logout)
